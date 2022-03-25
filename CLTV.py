2965e4b490203a70774bee7cc39d0e671ec99d8e
Bugra Varol <98893637+bugravarol@users.noreply.github.com>
2022-03-25 11:40:49 +0300

    Update CLTV.py

--- CLTV.py
+++ CLTV.py
@@ -55,7 +55,7 @@ def outlier_thresholds(dataframe, variable):
 
 def replace_with_thresholds(dataframe, variable):
     low_limit, up_limit = outlier_thresholds(dataframe, variable)
-    # dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
     dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
 
 ############################
